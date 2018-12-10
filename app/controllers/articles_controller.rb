f503aad4b13273d6f3170140ae8e714b5448de75
Your Name <you@example.com>
2018-12-10 18:40:19 +0800

    Added Delete function of CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
@@ -33,6 +34,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
  
   private
     def article_params
